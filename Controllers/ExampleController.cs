7261a8b361188e990d546322e5081b9cd147b472
tonyzhangshi81 <tonyzhangshi@163.com>
2022-02-27 19:49:51 +0800

    Example(DropDownList Linkage)

---
+++ Controllers/ExampleController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+using MvcMovie.Models;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ExampleController : Controller
+    {
+        private readonly ILogger<ExampleController> _logger;
+        private readonly MvcMovieContext _context;
+
+
+        public ExampleController(ILogger<ExampleController> logger, MvcMovieContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> Index(ExampleViewModel? viewModel)
+        {
+            if(viewModel is not null)
+            {
+                viewModel.GenresItems = new SelectList(await _context.Movie.ToListAsync(), "Genre", "Genre");
+                viewModel.TitleItems = new SelectList(_context.Movie.Where(d => d.Genre == viewModel.Genre).ToList(), "Title", "Title");
+                viewModel.GenreTitle = viewModel.Genre + viewModel.Title;
+
+                this.ModelState.Clear();
+                return View(viewModel);
+            }
+
+            var movieGenreVM = new ExampleViewModel
+            {
+                GenresItems = new SelectList(await _context.Movie.ToListAsync(), "Genre", "Genre"),
+            };
+
+            return View(movieGenreVM);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Genre"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult GetTitleList(string Genre)
+        {
+            var TitleItems = new SelectList(_context.Movie.Where(d => d.Genre == Genre).ToList(), "Title", "Title");
+
+            /*
+            if (CountryId == 1)
+            {
+                StateList = new List<State> { new State { StateId = 1, CountryId = 1, StateName = "Stockholm" }, new State { CountryId = 1, StateName = "Malmö", StateId = 2 }, new State { CountryId = 1, StateId = 3, StateName = "Göteborg" } };
+            }
+            else
+                StateList = new List<State> { new State { StateId = 1, CountryId = 1, StateName = "Oslo" } };
+            */
+            // , new Newtonsoft.Json.JsonSerializerSettings()
+
+            return Json(TitleItems);
+        }
+
+    }
+}
---
+++ Models/ExampleViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ExampleViewModel
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Display(Name = "MovieGenre")]
+        public IEnumerable<SelectListItem> GenresItems { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Display(Name = "TitleItems")]
+        public IEnumerable<SelectListItem> TitleItems { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Genre { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string GenreTitle { get; set; }
+
+    }
+}
